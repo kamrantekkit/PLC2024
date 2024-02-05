8bc65fef7de1b40caaa2fe38cc20c0f2bade5fbc
kamrantekkit <36983539+kamrantekkit@users.noreply.github.com>
2024-02-05 23:11:21 +0000

    core(fun as arg): updated task for fun as arg

--- lab2-functionalprimer/javascript/func_as_arg.js
+++ lab2-functionalprimer/javascript/func_as_arg.js
@@ -1,24 +1,23 @@
-//Create list of ints from 1 to 5, Haskell equivalent [1..5]
-function arrFunc(a, b){
+function arrFunc(a, b) {
     let arr = [];
-    for (let i = 1; i<=5; i++) {
+    for (let i = a; i <= b; i++) {
         arr.push(i);
     }
-    return arr;    
+    return arr;
 }
-
-function applicatorFunc(inpFunc, s){
-    if(s=='s'){
-        const arr = inpFunc;        
-        let sum = arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+function applicatorFunc(inpFunc, s, a, b) {
+    if (s == 's') {
+        const arr = inpFunc(a, b);
+        let sum = arr.reduce((accumulator, currentValue) =>
+            accumulator + currentValue, 0);
         return sum;
     }
-    else{        
-        const arr = inpFunc;
-        let sum = arr.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
-        return sum/5;
+    else {
+        const arr = inpFunc(a, b);
+        let sum = arr.reduce((accumulator, currentValue) =>
+            accumulator + currentValue, 0);
+        return sum / (b - a + 1);
     }
 }
-
-let x = applicatorFunc(arrFunc(1,10), 's');
+let x = applicatorFunc(arrFunc, 'a', 1, 10);
 console.log(x);
--- lab2-functionalprimer/python3/func_as_arg.py
+++ lab2-functionalprimer/python3/func_as_arg.py
@@ -1,10 +1,10 @@
-def listFunc():
-    return [i for i in range(1, 6)] #Create list of ints from 1 to 5, Haskell equivalent [1..5]
+def listFunc(a, b):
+ return [i for i in range(a, b+1)] #Create list of ints from 1 to 5, Haskell equivalent [1..5]
 
-def applicatorFunc(inpFunc, s):
-    if s=='s':
-        return sum(inpFunc())
-    else:
-        return sum(inpFunc())/5
 
-print(applicatorFunc(listFunc, 's'))
+def applicatorFunc(inpFunc, s, a, b):
+  if s=='s':
+    return sum(inpFunc(a, b))
+  else:
+    return sum(inpFunc(a, b))/(b-a+1)
+print(applicatorFunc(listFunc, 'a', 1, 10))
