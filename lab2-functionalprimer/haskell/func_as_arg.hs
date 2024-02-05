94e9ed7d8ce5e4d8404bf7709a952a7b8810d3da
kamrantekkit <36983539+kamrantekkit@users.noreply.github.com>
2024-02-05 22:09:15 +0000

    core: add alternative

--- lab2-functionalprimer/haskell/func_as_arg.hs
+++ lab2-functionalprimer/haskell/func_as_arg.hs
@@ -1,9 +1,12 @@
 --create inpFunc
 inpFunc a b = [a..b] 
 
---Define applicatorFunc
-applicatorFunc inpFunc s = if s=='s' then sum inpFunc else (sum inpFunc)/5  
+--Define applicatorFunc with if statements
+applicatorFunc inpFunc a b s = if s=='s' then sum inpFunc else  (sum inpFunc)/5  
+-- Using guards
+applicatorFunc2 inpFunc a b s | s =='s' = sum (inpFunc a b ) | otherwise = (sum inpFunc a b)/(b-a+1)
+
 
 main = do
-    let result = applicatorFunc (inpFunc 1 5) 'a' --Call applicatorFunc with inpFunc and 'a' as args
+    let result = applicatorFunc inpFunc 1 5 'a' --Call applicatorFunc with inpFunc and 'a' as args
     putStrLn("sum = " ++ show(result))
